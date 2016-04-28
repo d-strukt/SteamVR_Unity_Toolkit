f7361b5fd79ac4642213c44e6dcb225dffaf7d68
Theston E. Fox <thestonefox@users.noreply.github.com>
2016-04-28 07:20:15 +0100

    feat(Pointer): add bezier curve pointer
    The Bezier Pointer always shows a cursor on the top of any object and
    draws a bezier curve to the ground cursor so it is easier to see where
    the teleport location will end up.

---
+++ Assets/SteamVR_Unity_Toolkit/Scripts/Helper/CurveGenerator.cs
@@ -0,0 +1,281 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class Bezier
+{
+
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            oneMinusT * oneMinusT * p0 +
+            2f * oneMinusT * t * p1 +
+            t * t * p2;
+    }
+
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        return
+            2f * (1f - t) * (p1 - p0) +
+            2f * t * (p2 - p1);
+    }
+
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float OneMinusT = 1f - t;
+        return
+            OneMinusT * OneMinusT * OneMinusT * p0 +
+            3f * OneMinusT * OneMinusT * t * p1 +
+            3f * OneMinusT * t * t * p2 +
+            t * t * t * p3;
+    }
+
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            3f * oneMinusT * oneMinusT * (p1 - p0) +
+            6f * oneMinusT * t * (p2 - p1) +
+            3f * t * t * (p3 - p2);
+    }
+}
+
+public class CurveGenerator : MonoBehaviour
+{
+    enum BezierControlPointMode
+    {
+        Free,
+        Aligned,
+        Mirrored
+    }
+
+    Vector3[] points;
+    GameObject[] items;
+    BezierControlPointMode[] modes;
+    bool loop;
+    int frequency;
+
+    public void Create(int setFrequency)
+    {
+        frequency = setFrequency;
+
+        items = new GameObject[frequency];
+        for (int f = 0; f < items.Length; f++)
+        {
+            items[f] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            items[f].transform.parent = null;
+            Destroy(items[f].GetComponent<SphereCollider>());
+            items[f].layer = 2;
+            items[f].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+
+    public void SetPoints(Vector3[] controlPoints)
+    {
+        points = controlPoints;
+        modes = new BezierControlPointMode[] {
+            BezierControlPointMode.Free,
+            BezierControlPointMode.Free
+        };
+
+        SetObjects();
+    }
+
+    public void TogglePoints(bool state)
+    {
+        if(items == null)
+        {
+            return;
+        }
+        foreach(GameObject item in items)
+        {
+            item.SetActive(state);
+        }
+    }
+
+    bool Loop
+    {
+        get
+        {
+            return loop;
+        }
+        set
+        {
+            loop = value;
+            if (value == true)
+            {
+                modes[modes.Length - 1] = modes[0];
+                SetControlPoint(0, points[0]);
+            }
+        }
+    }
+
+    int ControlPointCount
+    {
+        get
+        {
+            return points.Length;
+        }
+    }
+
+    Vector3 GetControlPoint(int index)
+    {
+        return points[index];
+    }
+
+    void SetControlPoint(int index, Vector3 point)
+    {
+        if (index % 3 == 0)
+        {
+            Vector3 delta = point - points[index];
+            if (loop)
+            {
+                if (index == 0)
+                {
+                    points[1] += delta;
+                    points[points.Length - 2] += delta;
+                    points[points.Length - 1] = point;
+                }
+                else if (index == points.Length - 1)
+                {
+                    points[0] = point;
+                    points[1] += delta;
+                    points[index - 1] += delta;
+                }
+                else
+                {
+                    points[index - 1] += delta;
+                    points[index + 1] += delta;
+                }
+            }
+            else
+            {
+                if (index > 0)
+                {
+                    points[index - 1] += delta;
+                }
+                if (index + 1 < points.Length)
+                {
+                    points[index + 1] += delta;
+                }
+            }
+        }
+        points[index] = point;
+        EnforceMode(index);
+    }
+
+    void EnforceMode(int index)
+    {
+        int modeIndex = (index + 1) / 3;
+        BezierControlPointMode mode = modes[modeIndex];
+        if (mode == BezierControlPointMode.Free || !loop && (modeIndex == 0 || modeIndex == modes.Length - 1))
+        {
+            return;
+        }
+
+        int middleIndex = modeIndex * 3;
+        int fixedIndex, enforcedIndex;
+        if (index <= middleIndex)
+        {
+            fixedIndex = middleIndex - 1;
+            if (fixedIndex < 0)
+            {
+                fixedIndex = points.Length - 2;
+            }
+            enforcedIndex = middleIndex + 1;
+            if (enforcedIndex >= points.Length)
+            {
+                enforcedIndex = 1;
+            }
+        }
+        else
+        {
+            fixedIndex = middleIndex + 1;
+            if (fixedIndex >= points.Length)
+            {
+                fixedIndex = 1;
+            }
+            enforcedIndex = middleIndex - 1;
+            if (enforcedIndex < 0)
+            {
+                enforcedIndex = points.Length - 2;
+            }
+        }
+
+        Vector3 middle = points[middleIndex];
+        Vector3 enforcedTangent = middle - points[fixedIndex];
+        if (mode == BezierControlPointMode.Aligned)
+        {
+            enforcedTangent = enforcedTangent.normalized * Vector3.Distance(middle, points[enforcedIndex]);
+        }
+        points[enforcedIndex] = middle + enforcedTangent;
+    }
+
+    int CurveCount
+    {
+        get
+        {
+            return (points.Length - 1) / 3;
+        }
+    }
+
+    Vector3 GetPoint(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+        return transform.TransformPoint(Bezier.GetPoint(points[i], points[i + 1], points[i + 2], points[i + 3], t));
+    }
+
+    Vector3 GetVelocity (float t) {
+         int i;
+         if (t >= 1f) {
+             t = 1f;
+             i = points.Length - 4;
+         }
+         else {
+             t = Mathf.Clamp01(t) * CurveCount;
+             i = (int)t;
+             t -= i;
+             i *= 3;
+         }
+         return transform.TransformPoint(Bezier.GetFirstDerivative(points[i], points[i + 1], points[i + 2], points[i + 3], t)) - transform.position;
+     }
+
+    Vector3 GetDirection (float t) {
+		return GetVelocity(t).normalized;
+	}
+
+    void SetObjects()
+    {
+        float stepSize = frequency * 1;
+        if (this.Loop || stepSize == 1)
+        {
+            stepSize = 1f / stepSize;
+        }
+        else
+        {
+            stepSize = 1f / (stepSize - 1);
+        }
+
+        for (int f = 0; f < frequency; f++)
+        {
+            Vector3 position = this.GetPoint(f * stepSize);
+            items[f].transform.position = position;
+        }
+    }
+}
---
+++ Assets/SteamVR_Unity_Toolkit/Scripts/SteamVR_BezierPointer.cs
@@ -0,0 +1,220 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteamVR_BezierPointer : SteamVR_WorldPointer
+{
+    public enum AxisType
+    {
+        XAxis,
+        ZAxis
+    }
+
+    public Color pointerColor;
+    public float pointerLength = 10f;
+    public bool showPointerCursor = true;
+    public AxisType pointerFacingAxis = AxisType.ZAxis;
+
+    private Transform projectedBeamContainer;
+    private Transform projectedBeamForward;
+    private Transform projectedBeamJoint;
+    private Transform projectedBeamDown;
+
+    private GameObject pointerCursor;
+
+    private float pointerContactDistance = 0f;
+    private Transform pointerContactTarget = null;
+
+    private uint controllerIndex;
+    private CurveGenerator curvedBeam;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (GetComponent<SteamVR_ControllerEvents>() == null)
+        {
+            Debug.LogError("SteamVR_SimplePointer is required to be attached to a SteamVR Controller that has the SteamVR_ControllerEvents script attached to it");
+            return;
+        }
+
+        //Setup controller event listeners
+        GetComponent<SteamVR_ControllerEvents>().AliasPointerOn += new ControllerClickedEventHandler(EnablePointerBeam);
+        GetComponent<SteamVR_ControllerEvents>().AliasPointerOff += new ControllerClickedEventHandler(DisablePointerBeam);
+
+        InitProjectedBeams();
+        InitPointer();
+        TogglePointer(false);
+    }
+
+    void InitProjectedBeams()
+    {
+        projectedBeamContainer = new GameObject().transform;
+        projectedBeamContainer.transform.parent = this.transform;
+        projectedBeamContainer.transform.localPosition = Vector3.zero;
+
+        projectedBeamForward = new GameObject().transform;
+        projectedBeamForward.transform.parent = projectedBeamContainer.transform;
+
+        projectedBeamJoint = new GameObject().transform;
+        projectedBeamJoint.transform.parent = projectedBeamContainer.transform;
+        projectedBeamJoint.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+        projectedBeamDown = new GameObject().transform;
+    }
+
+    void InitPointer()
+    {
+        Material newMaterial = new Material(Shader.Find("Unlit/Color"));
+        newMaterial.SetColor("_Color", pointerColor);
+
+        pointerCursor = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        pointerCursor.GetComponent<MeshRenderer>().material = newMaterial;
+        pointerCursor.transform.localScale = new Vector3(0.5f, 0.02f, 0.5f);
+
+        Destroy(pointerCursor.GetComponent<CapsuleCollider>());
+
+        pointerCursor.AddComponent<BoxCollider>();
+        pointerCursor.GetComponent<BoxCollider>().isTrigger = true;
+        pointerCursor.AddComponent<Rigidbody>().isKinematic = true;
+        pointerCursor.layer = 2;
+
+        GameObject global = new GameObject();
+        curvedBeam = global.gameObject.AddComponent<CurveGenerator>();
+        curvedBeam.transform.parent = null;
+        curvedBeam.Create(10);
+    }
+
+    float GetForwardBeamLength()
+    {
+        float actualLength = pointerLength;
+        Ray pointerRaycast = new Ray(transform.position, transform.forward);
+        RaycastHit collidedWith;
+        bool hasRayHit = Physics.Raycast(pointerRaycast, out collidedWith);
+
+        //reset if beam not hitting or hitting new target
+        if (!hasRayHit || (pointerContactTarget && pointerContactTarget != collidedWith.transform))
+        {
+            pointerContactDistance = 0f;
+        }
+
+        //check if beam has hit a new target
+        if (hasRayHit)
+        {
+            pointerContactDistance = collidedWith.distance;
+        }
+
+        //adjust beam length if something is blocking it
+        if (hasRayHit && pointerContactDistance < pointerLength)
+        {
+            actualLength = pointerContactDistance;
+        }
+
+        return actualLength;
+    }
+
+    void ProjectForwardBeam()
+    {
+        float setThicknes = 0.01f;
+        float setLength = GetForwardBeamLength();
+        //if the additional decimal isn't added then the beam position glitches
+        float beamPosition = setLength / (2 + 0.00001f);
+
+        if (pointerFacingAxis == AxisType.XAxis)
+        {
+            projectedBeamForward.transform.localScale = new Vector3(setLength, setThicknes, setThicknes);
+            projectedBeamForward.transform.localPosition = new Vector3(beamPosition, 0f, 0f);
+            projectedBeamJoint.transform.localPosition = new Vector3(setLength - (projectedBeamJoint.transform.localScale.x / 2), 0f, 0f);
+        }
+        else
+        {
+            projectedBeamForward.transform.localScale = new Vector3(setThicknes, setThicknes, setLength);
+            projectedBeamForward.transform.localPosition = new Vector3(0f, 0f, beamPosition);
+            projectedBeamJoint.transform.localPosition = new Vector3(0f, 0f, setLength - (projectedBeamJoint.transform.localScale.z / 2));
+        }        
+    }
+
+    void ProjectDownBeam()
+    {
+        projectedBeamDown.transform.position = new Vector3(projectedBeamJoint.transform.position.x, projectedBeamJoint.transform.position.y, projectedBeamJoint.transform.position.z);
+
+        Ray projectedBeamDownRaycast = new Ray(projectedBeamDown.transform.position, Vector3.down);
+        RaycastHit collidedWith;
+        bool downRayHit = Physics.Raycast(projectedBeamDownRaycast, out collidedWith);
+
+        if (!downRayHit || (pointerContactTarget && pointerContactTarget != collidedWith.transform))
+        {
+            if (pointerContactTarget != null)
+            {
+                OnWorldPointerOut(SetPointerEvent(controllerIndex, pointerContactDistance, pointerContactTarget, projectedBeamDown.transform.position));
+            }
+            pointerContactTarget = null;
+        }
+
+        if (downRayHit)
+        {
+            projectedBeamDown.transform.position = new Vector3(projectedBeamJoint.transform.position.x, projectedBeamJoint.transform.position.y - collidedWith.distance, projectedBeamJoint.transform.position.z);
+            projectedBeamDown.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            pointerContactTarget = collidedWith.transform;
+            OnWorldPointerIn(SetPointerEvent(controllerIndex, pointerContactDistance, pointerContactTarget, projectedBeamDown.transform.position));
+        }
+    }
+
+    void SetPointerCursor()
+    {
+        if (pointerContactTarget != null)
+        {
+            pointerCursor.gameObject.SetActive(showPointerCursor);
+            pointerCursor.transform.position = projectedBeamDown.transform.position;
+        } else
+        {
+            pointerCursor.gameObject.SetActive(false);
+        }
+    }
+
+    void TogglePointer(bool state)
+    {
+        projectedBeamForward.gameObject.SetActive(state);
+        projectedBeamJoint.gameObject.SetActive(state);
+        projectedBeamDown.gameObject.SetActive(state);
+        curvedBeam.TogglePoints(state);
+
+        bool cursorState = (showPointerCursor ? state : false);
+        pointerCursor.gameObject.SetActive(cursorState);
+    }
+
+    void EnablePointerBeam(object sender, ControllerClickedEventArgs e)
+    {
+        controllerIndex = e.controllerIndex;
+        TogglePointer(true);
+    }
+
+    void DisablePointerBeam(object sender, ControllerClickedEventArgs e)
+    {
+        controllerIndex = e.controllerIndex;
+        OnWorldPointerDestinationSet(SetPointerEvent(controllerIndex, pointerContactDistance, pointerContactTarget, projectedBeamDown.transform.position));
+        TogglePointer(false);
+    }
+
+    void DisplayCurvedBeam()
+    {
+        Vector3[] beamPoints = new Vector3[]
+        {
+            this.transform.position,
+            projectedBeamJoint.transform.position,
+            projectedBeamDown.transform.position,
+            projectedBeamDown.transform.position,
+        };
+        curvedBeam.SetPoints(beamPoints);
+    }
+
+    void Update()
+    {
+        if (projectedBeamForward.gameObject.activeSelf)
+        {            
+            ProjectForwardBeam();
+            ProjectDownBeam();
+            DisplayCurvedBeam();
+            SetPointerCursor();
+        }
+    }
+}
